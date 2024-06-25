9a7f2ed0674308bdd1295aa861c10a67df62abb5
nowll <samuelcaesar704@gmail.com>
2024-06-25 14:23:11 +0700

    Change Port

--- server.go
+++ server.go
@@ -8,47 +8,44 @@ import (
 	"text/template"
 )
 
-func handlerIndex(w http.ResponseWriter, r *http.Request){
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome"))
 }
 
-func handlerPage(w http.ResponseWriter, r *http.Request){
-	filepath := path.Join("views","index.html")
+func handlerPage(w http.ResponseWriter, r *http.Request) {
+	filepath := path.Join("views", "index.html")
 
-	tmpl,err:= template.ParseFiles(filepath)
+	tmpl, err := template.ParseFiles(filepath)
 
-	if err!= nil {
-		helper.HttpErrorHandler(w,err)
-    }
-
-	data:= map[string]interface{}{
-		"title":"Hello",
-        "content":"Hello, world!",
+	if err != nil {
+		helper.HttpErrorHandler(w, err)
 	}
 
-	err = tmpl.Execute(w,data)
-	if err!= nil {
-       helper.HttpErrorHandler(w,err)
-    }
+	data := map[string]interface{}{
+		"title":   "Hello",
+		"content": "Hello, world!",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		helper.HttpErrorHandler(w, err)
+	}
 }
 
-
-
-func handlerHello(w http.ResponseWriter, r *http.Request){
+func handlerHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, world!"))
 }
 
-
 func main() {
 
-	http.Handle("/static/",http.StripPrefix("/static/",http.FileServer(http.Dir("assets"))))
-	http.HandleFunc("/",handlerPage)
-	http.HandleFunc("/hello",handlerHello)
-	address := "localhost:9000"
-	fmt.Printf("Server start at %s\n",address)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/", handlerPage)
+	http.HandleFunc("/hello", handlerHello)
+	address := ":80"
+	fmt.Printf("Server start at %s\n", address)
 
-	err:= http.ListenAndServe(address,nil)
-	if err != nil{
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
 		helper.ErrorHelper(err)
 	}
 }
